f3ada5220a533b66d4184dd1121b5cd5af160e53
george <george@fozzie.dev>
2022-03-17 15:35:39 +0000

    Change logging level for denied command results.

--- src/listeners/chatInputCommandDenied.ts
+++ src/listeners/chatInputCommandDenied.ts
@@ -8,6 +8,8 @@ import { replyInteractionError } from "../utility/Sender";
 export class ApplicationCommandDeniedListener extends Listener {
   public async run(error: UserError, { interaction }: ChatInputCommandDeniedPayload) {
     await replyInteractionError(interaction, error.message);
-    this.container.logger.error(`${interaction.user.tag} - ${error.message}`);
+    this.container.logger.info(
+      `Unsuccessful command result - ${interaction.user.tag} - ${error.message}`
+    );
   }
 }
