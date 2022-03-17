6ca6b41042f17f005f94eaa0096365735d0efe70
george <george@fozzie.dev>
2022-03-17 15:39:50 +0000

    Make clear that these are Marshal commands.

--- src/services/PermissionService.ts
+++ src/services/PermissionService.ts
@@ -4,21 +4,21 @@ import { ApplicationCommandPermissionData } from "discord.js";
 
 export class PermissionService {
   public static async register() {
-    const modCommands = ["954020435652128848"];
-    const modPermissions: Array<ApplicationCommandPermissionData> = [];
-    Constants.MOD_ROLES.filter((modRole) => modRole.permissionLevel > 0).forEach(
-      (modRole) => {
-        modPermissions.push({ id: modRole.id, type: "ROLE", permission: true });
-      }
-    );
+    const marshalCommands = ["954020435652128848"];
+    const marshalPermissions: Array<ApplicationCommandPermissionData> = [];
+    Constants.MOD_ROLES.filter(
+      (marshalRole) => marshalRole.permissionLevel > 0
+    ).forEach((marshalRole) => {
+      marshalPermissions.push({ id: marshalRole.id, type: "ROLE", permission: true });
+    });
     const guild = container.client.guilds.cache.get(Constants.GUILD_IDS[0]);
     if (guild == null) {
       return;
     }
-    for (const commandId of modCommands) {
+    for (const commandId of marshalCommands) {
       const command = await guild.commands.fetch(commandId);
       if (command != null) {
-        await command.permissions.add({ permissions: modPermissions });
+        await command.permissions.add({ permissions: marshalPermissions });
       }
     }
   }
