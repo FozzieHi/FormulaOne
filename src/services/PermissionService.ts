2d9018fb14b12db363ec7729798f7cbb8b15ef36
george <george@fozzie.dev>
2022-03-22 14:04:47 +0000

    Cleanup PermissionService.

--- src/services/PermissionService.ts
+++ src/services/PermissionService.ts
@@ -1,5 +1,5 @@
 import { container } from "@sapphire/framework";
-import { ApplicationCommandPermissionData, Snowflake } from "discord.js";
+import {ApplicationCommandPermissionData, Guild, Snowflake} from "discord.js";
 import { Constants } from "../utility/Constants";
 
 export class PermissionService {
@@ -11,39 +11,37 @@ export class PermissionService {
       "954752295218843728",
       "955436856516948008",
     ];
-    const stewardsCommands: Array<Snowflake> = [
+    const stewardCommands: Array<Snowflake> = [
       "955204850902265986",
       "955230109890146404",
     ];
-    const marshalPermissions: Array<ApplicationCommandPermissionData> = [];
-    Constants.MOD_ROLES.filter((role) => role.permissionLevel > 0).forEach(
-      (marshalRole) => {
-        marshalPermissions.push({ id: marshalRole.id, type: "ROLE", permission: true });
-      }
-    );
-    const stewardPermissions: Array<ApplicationCommandPermissionData> = [];
-    Constants.MOD_ROLES.filter((role) => role.permissionLevel > 1).forEach(
-      (stewardRole) => {
-        stewardPermissions.push({ id: stewardRole.id, type: "ROLE", permission: true });
-      }
-    );
+    const marshalPermissions = this.getPermData(1);
+    const stewardPermissions = this.getPermData(2);
     const guild = container.client.guilds.cache.get(Constants.GUILD_IDS[0]);
     if (guild == null) {
       return;
     }
-    await Promise.all(
-      (
-        await Promise.all(
-          marshalCommands.map((commandId) => guild.commands.fetch(commandId))
-        )
-      ).map((command) => command.permissions.set({ permissions: marshalPermissions }))
+    await this.setPerms(guild, marshalCommands, marshalPermissions);
+    await this.setPerms(guild, stewardCommands, stewardPermissions);
+  }
+
+  private static getPermData(permissionLevel: number) {
+    const permissions: Array<ApplicationCommandPermissionData> = [];
+    Constants.MOD_ROLES.filter((role) => role.permissionLevel >= permissionLevel).forEach(
+      (marshalRole) => {
+        permissions.push({ id: marshalRole.id, type: "ROLE", permission: true });
+      }
     );
+    return permissions;
+  }
+
+  private static async setPerms(guild: Guild, commands: Array<Snowflake>, permissions: Array<ApplicationCommandPermissionData>) {
     await Promise.all(
       (
         await Promise.all(
-          stewardsCommands.map((commandId) => guild.commands.fetch(commandId))
+          commands.map((commandId) => guild.commands.fetch(commandId))
         )
-      ).map((command) => command.permissions.set({ permissions: stewardPermissions }))
+      ).map((command) => command.permissions.set({ permissions }))
     );
   }
 }
