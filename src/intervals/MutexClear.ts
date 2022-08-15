485863b283900394ab64c682e3a22eedaee255cc
george <george@fozzie.dev>
2022-08-15 11:33:57 +0100

    Only assign used variable.

--- src/intervals/MutexClear.ts
+++ src/intervals/MutexClear.ts
@@ -1,11 +1,10 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import MutexManager from "../managers/MutexManager";
 import { Constants } from "../utility/Constants";
 
 setInterval(() => {
   [...MutexManager.mutexes]
     .filter(
-      ([_, value]) => Date.now() - value.lastUsed > 120000 && !value.mutex.isLocked()
+      ([, value]) => Date.now() - value.lastUsed > 120000 && !value.mutex.isLocked()
     )
     .forEach(([key]) => MutexManager.mutexes.delete(key));
 }, Constants.INTERVALS.MUTEXES);
