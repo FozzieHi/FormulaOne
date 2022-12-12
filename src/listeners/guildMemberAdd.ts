4ea529e00aa0d95f46d32e06b386cde3fd0e3832
george <george@fozzie.dev>
2022-12-12 19:45:47 +0000

    Add welcome message.

--- src/listeners/guildMemberAdd.ts
+++ src/listeners/guildMemberAdd.ts
@@ -1,6 +1,10 @@
-import { GuildMember } from "discord.js";
+import { GuildMember, GuildTextBasedChannel } from "discord.js";
 import { Listener } from "@sapphire/framework";
 import ProtectionService from "../services/ProtectionService.js";
+import { send } from "../utility/Sender.js";
+import { boldify } from "../utility/StringUtil.js";
+import { Constants } from "../utility/Constants.js";
+import TryVal from "../utility/TryVal.js";
 
 export class GuildMemberAddListener extends Listener {
   public constructor(context: Listener.Context) {
@@ -9,5 +13,18 @@ export class GuildMemberAddListener extends Listener {
 
   public async run(member: GuildMember) {
     await ProtectionService.checkJoins(member.guild);
+
+    const welcomeChannel = (await TryVal(
+      member.guild.channels.fetch(Constants.CHANNELS.WELCOME)
+    )) as GuildTextBasedChannel;
+    if (welcomeChannel != null) {
+      await send(
+        welcomeChannel,
+        `Hello ${boldify(member.user.tag)}, welcome to the r/formula1 Discord server!` +
+          ` You should be given access to the server after you read and agree to the <#${Constants.CHANNELS.RULES}>.` +
+          `\n\nDM <@${Constants.BOTS.MOD_MAIL}> if you have issues after verifying and refreshing your browser / restarting the app.` +
+          `\n\nSay hi in <#${Constants.CHANNELS.OFF_TOPIC}> or dive right into F1 chat in <#${Constants.CHANNELS.F1_GENERAL}>. We look forward to talking to you.`
+      );
+    }
   }
 }
