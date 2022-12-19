c274ad492fc5135443950bc43e3fea0303b1eb47
george <george@fozzie.dev>
2022-12-19 21:27:05 +0000

    Capture errors on listener.

--- src/listeners/contextMenuCommandError.ts
+++ src/listeners/contextMenuCommandError.ts
@@ -3,13 +3,15 @@ import {
   Listener,
   UserError,
 } from "@sapphire/framework";
+import Sentry from "@sentry/node";
 import { replyInteractionError } from "../utility/Sender.js";
 
 export class ContextMenuCommandErrorListener extends Listener {
   public async run(error: UserError, { interaction }: ContextMenuCommandErrorPayload) {
     await replyInteractionError(interaction, error.message);
-    this.container.logger.info(
+    this.container.logger.error(
       `Unsuccessful (error) context menu result - ${interaction.user.tag} - ${interaction.commandName} - ${error.message}`
     );
+    Sentry.captureException(error);
   }
 }
--- src/listeners/interactionHandlerError.ts
+++ src/listeners/interactionHandlerError.ts
@@ -2,11 +2,11 @@ import { ChatInputCommandErrorPayload, Listener, UserError } from "@sapphire/fra
 import Sentry from "@sentry/node";
 import { replyInteractionError } from "../utility/Sender.js";
 
-export class ChatInputCommandDeniedListener extends Listener {
+export class InteractionHandlerErrorListener extends Listener {
   public async run(error: UserError, { interaction }: ChatInputCommandErrorPayload) {
     await replyInteractionError(interaction, error.message);
     this.container.logger.error(
-      `Unsuccessful (error) command result - ${interaction.user.tag} - ${interaction.commandName} - ${error.message}`
+      `Unsuccessful (error) interaction handler result - ${interaction.user.tag} - ${interaction.commandName} - ${error.message}`
     );
     Sentry.captureException(error);
   }
