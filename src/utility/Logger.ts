d673566466dfcbde3f3794127a81a10821eae119
george <george@fozzie.dev>
2023-01-02 14:15:40 +0000

    Change event type.

--- src/utility/Logger.ts
+++ src/utility/Logger.ts
@@ -1,7 +1,7 @@
 import { container } from "@sapphire/framework";
 import Sentry from "@sentry/node";
 
-export function handleError(error: Error) {
+export function handleError(error: unknown) {
   container.logger.error(error);
   Sentry.captureException(error);
 }
