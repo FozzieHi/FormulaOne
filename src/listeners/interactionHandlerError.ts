5ce0f9ae13848872a0afd98d9ad34267f4d0ac1c
george <george@fozzie.dev>
2023-01-02 14:39:48 +0000

    Check if interaction is repliable

--- src/listeners/interactionHandlerError.ts
+++ src/listeners/interactionHandlerError.ts
@@ -1,8 +1,12 @@
-import { Listener } from "@sapphire/framework";
+import { IInteractionHandlerPayload, Listener } from "@sapphire/framework";
 import Sentry from "@sentry/node";
+import { replyInteractionError } from "../utility/Sender.js";
 
 export class InteractionHandlerErrorListener extends Listener {
-  public async run(error: Error) {
+  public async run(error: Error, { interaction }: IInteractionHandlerPayload) {
+    if (interaction.isRepliable()) {
+      await replyInteractionError(interaction as never, error.message);
+    }
     Sentry.captureException(error);
   }
 }
