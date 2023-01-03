0634ec50292736cb5098259498b6905952d15865
george <george@fozzie.dev>
2023-01-03 19:29:52 +0000

    Check if interaction has been replied to

--- src/listeners/interactionHandlerError.ts
+++ src/listeners/interactionHandlerError.ts
@@ -1,10 +1,11 @@
 import { IInteractionHandlerPayload, Listener } from "@sapphire/framework";
 import Sentry from "@sentry/node";
+import { BaseCommandInteraction } from "discord.js";
 import { replyInteractionError } from "../utility/Sender.js";
 
 export class InteractionHandlerErrorListener extends Listener {
   public async run(error: Error, { interaction }: IInteractionHandlerPayload) {
-    if (interaction.isRepliable()) {
+    if (!(interaction as BaseCommandInteraction)?.replied) {
       await replyInteractionError(interaction as never, error.message);
     }
     Sentry.captureException(error);
