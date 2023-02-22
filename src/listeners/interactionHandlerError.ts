b47fa40a7e40208abfdd2d13f6693d0734f05a60
george <george@fozzie.dev>
2023-02-22 19:31:43 +0000

    Check interaction before replying

--- src/listeners/interactionHandlerError.ts
+++ src/listeners/interactionHandlerError.ts
@@ -5,7 +5,7 @@ import { replyInteractionError } from "../utility/Sender.js";
 
 export class InteractionHandlerErrorListener extends Listener {
   public async run(error: Error, { interaction }: IInteractionHandlerPayload) {
-    if (!(interaction as CommandInteraction)?.replied) {
+    if (interaction != null && !(interaction as CommandInteraction).replied) {
       await replyInteractionError(interaction as never, error.message);
     }
     Sentry.captureException(error);
