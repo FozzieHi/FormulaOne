50ce138b58b26cd1da5c10d5fa9853a5a5a43243
Jackson Humphrey <jackson.s.humphrey@gmail.com>
2023-08-12 11:39:33 -0500

    Disabled the automatic addition of upvotes/downvotes in f1-discussion/f1-tech.

--- src/listeners/messageCreate.ts
+++ src/listeners/messageCreate.ts
@@ -1,14 +1,9 @@
 import { Listener } from "@sapphire/framework";
 import { Message } from "discord.js";
 import { filterCheckMessage } from "../services/BotQueueService.js";
-import { Constants } from "../utility/Constants.js";
-import { addDiscussionEmotes } from "../utility/DiscussionService.js";
 
 export class MessageCreateListener extends Listener {
   public async run(message: Message) {
     await filterCheckMessage(message);
-    if (message.channel.id === Constants.CHANNELS.F1_DISCUSSION) {
-      await addDiscussionEmotes(message);
-    }
   }
 }
