126b4f5fc9966de08c64362aefacc37aeea54211
George <george@fozzie.dev>
2024-03-09 20:19:05 +0000

    Try and fetch the message

--- src/listeners/messageReactionAdd.ts
+++ src/listeners/messageReactionAdd.ts
@@ -2,12 +2,16 @@ import { Listener } from "@sapphire/framework";
 import { MessageReaction } from "discord.js";
 import { checkEmotes } from "../services/FilterService.js";
 import { checkDiscussionEmotes } from "../utility/DiscussionService.js";
+import TryVal from "../utility/TryVal.js";
 
 export class MessageReactionAddListener extends Listener {
   public async run(reaction: MessageReaction) {
     const message = reaction.message.partial
-      ? await reaction.message.fetch()
+      ? await TryVal(reaction.message.fetch())
       : reaction.message;
+    if (message == null) {
+      return;
+    }
 
     await checkEmotes(message, reaction);
     await checkDiscussionEmotes(message, reaction);
