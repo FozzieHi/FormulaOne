ee31a87a7bd1d4e181063cb50095955c22f58a29
George <george@fozzie.dev>
2024-03-09 14:45:34 +0000

    Add return type

--- src/utility/CommandUtil.ts
+++ src/utility/CommandUtil.ts
@@ -1,6 +1,7 @@
+import { ApplicationCommandOptionChoiceData } from "discord.js";
 import { Constants } from "./Constants.js";
 
-export function getRuleChoices() {
+export function getRuleChoices(): Array<ApplicationCommandOptionChoiceData<string>> {
   return Object.entries(Constants.RULES).map(([name, rule]) => ({
     name: `${name} - ${rule}`,
     value: name,
