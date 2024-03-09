e25decb4baf021d01b5cab52cd0c2c08c637d326
George <george@fozzie.dev>
2024-03-09 13:04:30 +0000

    Cleanup MutexClear

--- src/intervals/MutexClear.ts
+++ src/intervals/MutexClear.ts
@@ -2,9 +2,11 @@ import MutexManager from "../managers/MutexManager.js";
 import { Constants } from "../utility/Constants.js";
 
 setInterval(() => {
-  [...MutexManager.userMutexes]
-    .filter(
-      ([, value]) => Date.now() - value.lastUsed > 120000 && !value.mutex.isLocked(),
-    )
-    .forEach(([key]) => MutexManager.userMutexes.delete(key));
+  const currentTime = Date.now();
+
+  MutexManager.userMutexes.forEach((value, key) => {
+    if (currentTime - value.lastUsed > 120000 && !value.mutex.isLocked()) {
+      MutexManager.userMutexes.delete(key);
+    }
+  });
 }, Constants.INTERVALS.MUTEX_CLEAR);
