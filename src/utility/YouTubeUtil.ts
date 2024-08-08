6b6fa8c40ed5bc3173ae4b2a466d24f64a470a80
George <george@fozzie.dev>
2024-08-08 22:32:32 +0100

    Cleanup YouTubeUtil

--- src/utility/YouTubeUtil.ts
+++ src/utility/YouTubeUtil.ts
@@ -6,48 +6,53 @@ import { handleError } from "./Logger.js";
 const youtube = google.youtube("v3");
 
 export default async function lookupYouTubeChannels(ids: string[]) {
-  let idsFiltered = ids;
-  const results = [];
+  const idsNotInCache: string[] = [];
+  const results: { videoTitle: string; channelId: string }[] = [];
 
-  for (let i = 0; i < ids.length; i += 1) {
-    // eslint-disable-next-line no-await-in-loop
-    const result = (await db.youtubeChannelCacheRepo?.findEntry(
-      ids[i],
-    )) as YouTubeChannelCache;
+  const resultPromises = ids.map(
+    (id) =>
+      db.youtubeChannelCacheRepo?.findEntry(id) as Promise<YouTubeChannelCache | null>,
+  );
+  const resultsArray = await Promise.all(resultPromises);
+
+  resultsArray.forEach((result, i) => {
     if (result) {
       results.push({
         videoTitle: result.videoTitle,
         channelId: result.channelId,
       });
-      idsFiltered = idsFiltered.filter((item) => item !== ids[i]);
+    } else {
+      idsNotInCache.push(ids[i]);
     }
-  }
+  });
 
-  if (idsFiltered.length > 0) {
-    const request = {
-      part: ["snippet"],
-      id: idsFiltered,
-      key: process.env.YOUTUBE_DATA_API_KEY as string,
-    };
-
+  if (idsNotInCache.length > 0) {
     const response = await youtube.videos
-      .list(request)
+      .list({
+        part: ["snippet"],
+        id: idsNotInCache,
+        key: process.env.YOUTUBE_DATA_API_KEY as string,
+      })
       .catch((err) => handleError(err));
 
-    if (response?.data?.items) {
-      for (let i = 0; i < response.data.items.length; i += 1) {
-        const videoTitle = response.data.items[i].snippet?.title;
-        const channelId = response.data.items[i].snippet?.channelId;
-        if (videoTitle && channelId) {
-          // eslint-disable-next-line no-await-in-loop
-          await db.youtubeChannelCacheRepo?.insertEntry(
-            idsFiltered[i],
-            videoTitle,
-            channelId,
-          );
-          results.push({ videoTitle, channelId });
-        }
-      }
+    if (response?.data?.items != null) {
+      const cachePromises = response.data.items
+        .map((item, i) => {
+          const videoTitle = item.snippet?.title;
+          const channelId = item.snippet?.channelId;
+          if (videoTitle != null && channelId != null) {
+            results.push({ videoTitle, channelId });
+            return db.youtubeChannelCacheRepo?.insertEntry(
+              idsNotInCache[i],
+              videoTitle,
+              channelId,
+            );
+          }
+          return undefined;
+        })
+        .filter(Boolean);
+
+      await Promise.all(cachePromises);
     }
   }
 
